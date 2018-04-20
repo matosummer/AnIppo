10e7ddc7aef0ef9e864180d283bb8cad39d2c4b4
unknown <uesugi.chesster@gmail.com>
2018-04-21 00:24:01 +0700

    Update internetpositifpage, Add shortcut & button for reload https

--- anippo.js
+++ anippo.js
@@ -3,21 +3,29 @@ function CreateLayoutDoc(url) {
   let main = doc.createElement("main")
   let page = doc.createElement("p")
   let br = doc.createElement("br")
-  page.innerHTML = "Internet positif berulah mau di redirect, use vpn ato coba cek https"
+  page.innerHTML = "Internet positif berulah mau di redirect, use vpn ato coba cek https <br> pencet <strong>R</strong> untuk coba refresh dengan protocol https"
+  // Style
+  let cssStyle = `
+  .btn { background-color: #00BCD4;
+  width: 200px; 
+  height: 50px;
+  border: none;
+  margin-bottom: 20px;
+  color: white;
+  font-size: 1.4em;}`
+  let style = doc.createElement('style')
+  style.appendChild(doc.createTextNode(cssStyle))
   let button = doc.createElement("button")
-  button.style = `
-      background-color: #00BCD4;
-      width: 200px; 
-      height: 50px;
-      border: none;
-      margin-bottom: 20px;
-      color: white;
-      font-size: 1.4em;`
+  button.className = 'btn'
+  button.id = "btn_copyurl"
   let textArea = doc.createElement("textarea")
   textArea.textContent = url
   textArea.select()
   button.innerHTML = "Copy Url"
-  
+  let buttonRefreshHttps = button.cloneNode()
+  buttonRefreshHttps.innerHTML = "Try HTTPS"
+  buttonRefreshHttps.id = "btn_refreshhttps"
+
   let hate = doc.createElement("p")
   hate.innerHTML = "❤️ INETPOSITIP ❤️"
 
@@ -25,14 +33,28 @@ function CreateLayoutDoc(url) {
   script.type = "text/javascript"
   script.innerHTML = `//<![CDATA[
     document.addEventListener("DOMContentLoaded", () => {
-      document.querySelector("button").addEventListener("click", () => {
+      document.querySelector("#btn_copyurl").addEventListener("click", () => {
         document.querySelector("textarea").select()
         document.execCommand("copy")
       })
+      document.querySelector("#btn_refreshhttps").addEventListener("click", () => {
+        TryHttps()
+      })
+      document.addEventListener("keydown", (e) => {
+        if(e.keyCode == 82) {
+          TryHttps()
+        }
+      })
     })
+    function TryHttps() {
+      location.protocol = "https:";
+    }
   //]]> `
+  doc.head.appendChild(style)
   doc.head.appendChild(script)
   main.appendChild(page)
+  main.appendChild(buttonRefreshHttps)
+  main.appendChild(br.cloneNode())
   main.appendChild(button)
   main.appendChild(br)
   main.appendChild(textArea)
@@ -47,7 +69,7 @@ function listener(details) {
 
   // Block ads host
   if(
-    details.url.includes("cfs.uzone.id") ||
+    details.url.includes("uzone.id") ||
     details.url.includes("notifa.info")
   ) {
     return { cancel:true }
@@ -65,22 +87,35 @@ browser.webRequest.onBeforeRequest.addListener(
 function listenerInternetPositif(details) {
   if(details.statusCode == 200) {
     let filter = browser.webRequest.filterResponseData(details.requestId)
+    let inetPositip = false
     filter.ondata = event => {
       let eventData = event.data
-      // Inet positif page kinda always same, and byte length is less than 4000
-      if(event.data.byteLength >= 800 && event.data.byteLength <= 4000) {
+      // check bytelength of internetpositif page
+      if(event.data.byteLength >= 20000 && event.data.byteLength <= 80000) {
         let decoder = new TextDecoder("utf-8")
-        // Sometimes the page get injected by ads so the byte length increased
-        let str = decoder.decode(event.data.slice(event.data.byteLength - 2100, event.data.byteLength))
-        let title = "window.location.replace(\"http://internetpositif.uzone.id"
-        if(str.includes(title)) {
-          str = CreateLayoutDoc(details.url)
-          let encoder = new TextEncoder()
-          eventData = encoder.encode(str)
+        let str = decoder.decode(event.data.slice(0, 2000))
+        let domParser = new DOMParser()
+        let doc = domParser.parseFromString(str, "text/html")
+        let metaDesc = doc.head.querySelector("[name=description]").content
+        if(
+            doc.title === "Internet Positif" &&
+            metaDesc === "Internet Positif"
+          ) 
+        {
+            str = CreateLayoutDoc(details.url)
+            let encoder = new TextEncoder()
+            eventData = encoder.encode(str)
+            inetPositip = true
         }
       }
+
       filter.write(eventData)
-      filter.disconnect()
+
+      if(inetPositip)
+        filter.close()
+      else
+        filter.disconnect()
+      
     }
   }
 }
